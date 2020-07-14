ad8730712b6fb403dd9132749124f2b3f97162e8
pabloApoca <pablo_mtv08@hotmail.com>
2020-07-13 23:33:07 -0300

    Hice la seccion Sobre mi con datos pertinentes

--- js/menuNavegador.js
+++ js/menuNavegador.js
@@ -46,11 +46,52 @@ $(document).on('click', '.nav-menu a, .scrollto', function(e) {
     }
   });
 
+  //  Estado activo cuando la navegación este en desplazamiento
 
- 
- 
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 10;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 200) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  // Boton para volver al principio
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
 
 
+  // Efecto AOS
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out-back"
+  });
 
 
 })(jQuery);
